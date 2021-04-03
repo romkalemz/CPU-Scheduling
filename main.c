b12f2523d8ba5f0cbda415b2ee9ecbf98bdfa7d1
Roman <romkas2000@gmail.com>
2021-04-02 22:15:15 -0700

    Quantum int arg check + print statements

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 // struct to hold necessary command line arguments
 struct ARG {
-    char algo[4];
+    char algo[5];
     int quantum;
     char file_name[120];
     FILE* file_ptr;
@@ -17,8 +17,18 @@ int main(int argc, char** argv) {
         // if an error detected, exit program
         exit(1);
 
-    printf("algo: %s,   quantum: %i,    file name: %s\n", arg.algo, arg.quantum, arg.file_name);
 
+
+
+    // print necessary statements after each thread finishes
+    printf("\nInput File Name                 : %s\n", arg.file_name);
+    printf("CPU Scheduling Algorithm        : %s", arg.algo);
+    if(arg.quantum != 0)
+        printf(" (%i ms)\n", arg.quantum);
+    else
+        printf("\n");
+    printf("Avg. Turnaround time            : \n");
+    printf("Avg. Waiting time in ready queue: \n\n");
 
     return 0;
 }
@@ -32,6 +42,7 @@ int check_arg_input(int argc, char** argv) {
         return 1;
     }
     // loop through argv and set proper flag vars
+    char end = '\0';    // used to end algo and file_name strings
     for(int i = 1; i < argc; i++) {
         if(strcmp(argv[i], "-alg") == 0) {
            // check if algo name exists
@@ -40,11 +51,22 @@ int check_arg_input(int argc, char** argv) {
                 printf("\nERROR - INPROPER ARGUMENTS! -\n");
                 printf("Algorithm name \"%s\" does not exist!\n", argv[i+1]);
                 return 1;
-           } else
+           } else {
+               // initialize algorithm that was chosen
                 strcpy(arg.algo, argv[i+1]);
+                strcat(arg.algo, &end);
+           }
         } 
         else if(strcmp(argv[i], "-quantum") == 0) {
-            // check if quantum tag an int input
+            // check if quantum tag an int, not a LONG_MIN, and not a LONG_MAX
+            errno = 0;
+            char* temp = argv[i+2]; // use a temp endptr to check if no numbers were detected
+            arg.quantum = strtol(argv[i+1], &temp, 10); // convert string to int
+            if(errno == EINVAL || errno == ERANGE || argv[i+1] == temp) {
+                printf("\nERROR - INPROPER ARGUMENTS! -\n");
+                printf("Quantum input \"%s\" is not a valid input!\n\n", argv[i+1]);
+                return 1;
+            }
 
         }
         else if(strcmp(argv[i], "-input") == 0) {
@@ -53,8 +75,10 @@ int check_arg_input(int argc, char** argv) {
             if(arg.file_ptr == NULL) {
                 fprintf(stderr, "\nERROR - IMPROPER ARGUMENTS! -\n%s: %s\n\n", argv[i+1], strerror(errno));
                 return 1;
-            } else
+            } else {
                 strcpy(arg.file_name, argv[i+1]);
+                strcat(arg.file_name, &end);
+            }
         }
     }
     
