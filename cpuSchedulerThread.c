482e1ac9ecf2a8ca9938910e0b9b43d855427df4
Roman <romkas2000@gmail.com>
2021-04-06 13:08:28 -0700

    Threads seperated into files

---
+++ cpuSchedulerThread.c
@@ -0,0 +1,4 @@
+#include "inc.h"
+
+// CPU_scheduler_thread
+// Emulate by sleeping for given CPU burst time, then release, and place in IO_Q
---
+++ fileReadThread.c
@@ -0,0 +1,4 @@
+#include "inc.h"
+
+// FileRead_thread
+// Read the inputted file, create (emulated) process, place in Ready_Q
